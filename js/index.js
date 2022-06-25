469eee219745945627c24929f51b9d464b820a30
Darkmoon2039 <69280936+Darkmoon2039@users.noreply.github.com>
2022-06-25 13:52:32 -0700

    initial cv website files

---
+++ js/index.js
@@ -0,0 +1,14 @@
+document.querySelector(".navbar-toggler").toggle();
+
+
+$("#hide").onclick("click", function() {
+    $("#hide").toggle();
+})
+
+var Selector$4 = {
+    DATA_TOGGLE: '[data-toggle="dropdown"]',
+    FORM_CHILD: '.dropdown form',
+    MENU: '.dropdown-menu',
+    NAVBAR_NAV: '.navbar-nav',
+    VISIBLE_ITEMS: '.dropdown-menu .dropdown-item:not(.disabled):not(:disabled)'
+};
